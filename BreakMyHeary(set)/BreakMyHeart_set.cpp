7e9be85298341d792ce7d3a6d93c8435215d8bca
sin <godcharlie1018@gmail.com>
2019-12-01 11:53:06 +0800

    first commit
    You're a surgeon, you need to mend those girls' hearts. Therefore you need a data structure to support some functions.
    
    This question is about maintain a set that contains only integers.
    
    There will be n instructions.
    
    Each instruction will be either:
    
    insert:
    
    Insert a new element ai into the set. Note that if the element has already in the set, do nothing.
    
     
    
    print:
    
    print all the element in the set in increasing order.
    
     
    
    min:
    
    print the number in the set that is the smallest. If there's no number, do nothing.
    
     
    
    range_erase:
    
    You will have two integer l, r.
    
    You need to erase all the elements ai in the set that l <= ai <= r
    
     
    Hint: You can solve this question very fast by using std::set .
    
    These are some functions you may use in your code:
    
    set.begin(), set.size(), set.erase(), set.lower_bound(), set.upper_bound(), set.insert(). To traverse through your set, you may also need iterator or type auto(only in c++11!)

---
+++ BreakMyHeary(set)/BreakMyHeart_set.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<set>
+#include<string.h>
+using namespace std;
+
+int main(){
+    int lines;
+    cin>>lines;
+    set<int> set1;
+    while(lines--){
+        string command;
+        cin>>command;
+        if(command == "insert"){
+            int num;
+            cin>>num;
+            set1.insert(num);
+        }
+        else if(command == "print"){
+            if(set1.empty()) continue;
+            else{
+                cout<<*set1.begin();
+                for(auto i:set1){
+                    if(i == *set1.begin()) continue;
+                    cout<<" ";
+                    cout<<i;
+                }
+                cout<<endl;
+            }
+        }
+        else if(command == "min"){
+            if(set1.empty()) continue;
+            else{
+                auto i = set1.begin();
+                cout<<*i<<endl;
+            }
+        }
+        else if(command == "range_erase"){
+            int l,r;
+            cin>>l>>r;
+            set1.erase(set1.lower_bound(l),set1.upper_bound(r));
+        }
+
+    }
+}
